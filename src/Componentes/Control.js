20d77d1709abde5dbd5a4db7e326e695ba09917f
Omar Zavala <influencia.x.94@gmail.com>
2022-05-14 20:50:00 -0400

    Se agrega boton

--- src/Componentes/Control.js
+++ src/Componentes/Control.js
@@ -1,27 +1,29 @@
 import './Control.css';
+import Calculadora from './Calculadora';
+import Calorias from './Calorias';
 
-export default function Control(props) {
+export default function Control() {
+
+    //logica de los botones
+
+function One() {    
+    console.log('One');
+}
+
+function Two() {
+    console.log('Two');
+}
+
     return (
-        <form action="" className="control">
-            <input type="radio" name="botones" id="button_1" defaultValue="1" />
-            <label htmlFor="button_1" id="button_one">Calculadora</label>
-            <input type="radio" name="botones" id="button_2" defaultValue="2" />
-            <label htmlFor="button_2" id="button_two">Calorias</label>
-        </form>
+        <div>
+            <form action="" className="control">
+                <input type="radio" name="botones" id="button_1" defaultValue="1" onClick={One}/>
+                <label htmlFor="button_1" id="button_one">Calculadora</label>
+                <input type="radio" name="botones" id="button_2" defaultValue="2" onClick={Two}/>
+                <label htmlFor="button_2" id="button_two">Calorias</label>
+            </form>
+            <Calculadora />
+            <Calorias />
+        </div>
     );
-}
-
-//logica de los botones
-
-var botonUno = document.getElementById("button_1");
-var botonDos = document.getElementById("button_2");
-
-botonUno.onclick = function One() {    
-    document.getElementById("calcula").style = "display: flex";
-    document.getElementById("item_2").style = "display: none";
-}
-
-botonDos.onclick = function Two() {
-    document.getElementById("calcula").style = "display: none";
-    document.getElementById("item_2").style = "display: flex";
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+/* import App from './App'; */
 import reportWebVitals from './reportWebVitals';
 
-import Calculadora from './Componentes/Calculadora';
-import Calorias from './Componentes/Calorias';
+import Control from './Componentes/Control';
+/* import Calculadora from './Componentes/Calculadora';
+import Calorias from './Componentes/Calorias'; */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Calculadora />
-    <Calorias />
+    {/* <App /> */}
+    <Control />
+    {/* <Calculadora />
+    <Calorias /> */}
   </React.StrictMode>
 );
 
