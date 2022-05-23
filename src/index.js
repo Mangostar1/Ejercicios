33b6a536ade04a70816f106fe2e551b503c382a2
Omar Zavala <influencia.x.94@gmail.com>
2022-05-23 19:07:20 -0400

    Se agrega mi web, aun en produccion

--- src/index.js
+++ src/index.js
@@ -5,16 +5,12 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 import Control from './Componentes/Control';
-/* import Calculadora from './Componentes/Calculadora';
-import Calorias from './Componentes/Calorias'; */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <Control />
-    {/* <Calculadora />
-    <Calorias /> */}
   </React.StrictMode>
 );
 
