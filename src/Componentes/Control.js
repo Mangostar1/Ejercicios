bc7c7656339da9ff0d1ba5438b247a35db39dcb7
Omar Zavala <influencia.x.94@gmail.com>
2022-06-03 13:26:32 -0400

    Se inicia un nuevo componente al proyecto

--- src/Componentes/Control.js
+++ src/Componentes/Control.js
@@ -4,6 +4,7 @@ import './Control.css';
 import Calculadora from './Calculadora';
 import Calorias from './Calorias';
 import TicTacToe from './TicTacToe';
+import Paint from './Paint';
 
 export default class Control extends Component {
 
@@ -11,24 +12,30 @@ export default class Control extends Component {
         contador: 1,
     }
 //logica de los botones
-    One = (props) => {    
+    Calc = (props) => {    
         this.setState(
             {contador: 1}
         );
     }
 
-    Two = (props) => {
+    Cal = (props) => {
         this.setState(
             {contador: 2}
         );
     }
 
-    Three = (props) => {
+    TicTac = (props) => {
         this.setState(
             {contador: 3}
         );
     }
 
+    Paint = (props) => {
+        this.setState(
+            {contador: 4}
+        )
+    }
+//Control de contenidos con if
     ControlContenido = () => {
         if (this.state.contador === 1) {
             return <Calculadora />;
@@ -36,22 +43,24 @@ export default class Control extends Component {
             return <Calorias />;
         } else if (this.state.contador === 3) {
             return <TicTacToe />;
+        } else if (this.state.contador === 4) {
+            return <Paint />;
         }
     }
     render() {
         return (
             <div id="control">
                 <form action="" className="control">
-                    <input type="radio" name="botones" id="button_1" defaultValue="1" onClick={this.One} defaultChecked/>
+                    <input type="radio" name="botones" id="button_1" defaultValue="1" onClick={this.Calc} defaultChecked/>
                     <label htmlFor="button_1" id="button_one">Calculadora</label>
-                    <input type="radio" name="botones" id="button_2" defaultValue="2" onClick={this.Two}/>
+                    <input type="radio" name="botones" id="button_2" defaultValue="2" onClick={this.Cal}/>
                     <label htmlFor="button_2" id="button_two">Calorias</label>
-                    <input type="radio" name="botones" id="button_3" defaultValue="3" onClick={this.Three}/>
+                    <input type="radio" name="botones" id="button_3" defaultValue="3" onClick={this.TicTac}/>
                     <label htmlFor="button_3" id="button_three">Tic Tac Toe</label>
+                    <input type="radio" name="botones" id="button_4" defaultValue="4" onClick={this.Paint}/>
+                    <label htmlFor="button_4" id="button_four">Paint</label>
                 </form>
-                {
-                    this.ControlContenido()
-                }
+                {this.ControlContenido()}
             </div>
         );
     }
---
+++ src/Componentes/Paint.js
@@ -0,0 +1,12 @@
+import React,{ Component} from 'react'
+import './Paint.css'
+
+export default class Paint extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Paint</h1>
+            </div>
+        );
+    }
+}
