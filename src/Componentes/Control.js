986e2c354df111148e442296af87d76217785e1f
Omar Zavala <influencia.x.94@gmail.com>
2022-06-02 21:57:28 -0400

    Se finaliza la logica basica de la pagina hasta ahora

--- src/Componentes/Control.js
+++ src/Componentes/Control.js
@@ -1,29 +1,41 @@
+import React,{ Component } from 'react';
+
 import './Control.css';
 import Calculadora from './Calculadora';
 import Calorias from './Calorias';
 
-export default function Control(props) {
+export default class Control extends Component {
 
+    state = {
+        contador: 1,
+    }
 //logica de los botones
-    function One(props) {    
+    One = (props) => {    
         console.log('One');
+        this.setState(
+            {contador: 1}
+        );
     }
 
-    function Two(props) {
+    Two = (props) => {
         console.log('Two');
+        this.setState(
+            {contador: 2}
+        );
     }
-
-    return (
-        <div id="control">
-            <form action="" className="control">
-                <input type="radio" name="botones" id="button_1" defaultValue="1" onClick={One}/>
-                <label htmlFor="button_1" id="button_one">Calculadora</label>
-                <input type="radio" name="botones" id="button_2" defaultValue="2" onClick={Two}/>
-                <label htmlFor="button_2" id="button_two">Calorias</label>
-            </form>
-
-            <Calculadora />
-            <Calorias />
-        </div>
-    );
+    render() {
+        return (
+            <div id="control">
+                <form action="" className="control">
+                    <input type="radio" name="botones" id="button_1" defaultValue="1" onClick={this.One} defaultChecked/>
+                    <label htmlFor="button_1" id="button_one">Calculadora</label>
+                    <input type="radio" name="botones" id="button_2" defaultValue="2" onClick={this.Two}/>
+                    <label htmlFor="button_2" id="button_two">Calorias</label>
+                </form>
+                {
+                    this.state.contador === 1 ? <Calculadora /> : <Calorias />
+                }
+            </div>
+        );
+    }
 }
