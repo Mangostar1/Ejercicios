8ea2d1625e9c52f90ebea1439df68306b4c6e497
Omar Zavala <influencia.x.94@gmail.com>
2022-06-09 17:50:58 -0400

    Se guarda progreso

--- src/Componentes/TicTacToe.js
+++ src/Componentes/TicTacToe.js
@@ -1,10 +1,12 @@
 import React,{ Component} from 'react'
+import cross from "../img/x-lg-svgrepo-com.svg";
+import circle from "../img/circle-svgrepo-com.svg";
 import './TicTacToe.css'
 
 export default class TicTacToe extends Component {
     
     state = {
-        contador: 0,
+        ps1Play: 0,
         turno: '',
         celda1: '',
         celda2: '',
@@ -18,17 +20,18 @@ export default class TicTacToe extends Component {
         ganador: '',
         jugador: 0,
         ia: 0,
+        iaPlay: '',
     }
 
     circle = () => {
         this.setState(
-            {contador: 1}
+            {ps1Play: 2, iaPlay: 'cross'}
         );
     }
     
     cross = () => {
         this.setState(
-            {contador: 2}
+            {ps1Play: 1, iaPlay: 'circle'}
         );
     }
 
@@ -45,205 +48,225 @@ export default class TicTacToe extends Component {
     }
 
     Celda1 = () => {
-        if (this.state.contador === 1) {
+        if (this.state.ps1Play === 1) {
             console.log('celda 1 usa X');
             this.setState({
                 celda1: 'X',
                 turno: 'X',
             });
-            document.getElementById('celda1').style.backgroundColor = '#00FF00';
+            document.getElementById('celda1').style.cssText = `background-image: url("${cross}");`;
+            this.IA();
         
-        } else if (this.state.contador === 2) {
+        } else if (this.state.ps1Play === 2) {
             console.log('celda 1 usa O');
             this.setState({
                 celda1: 'O',
                 turno: 'O',
             });
-            document.getElementById('celda1').style.backgroundColor = '#FF0000';
+            document.getElementById('celda1').style.cssText = `background-image: url("${circle}");`;
+            this.IA();
         
         } else {
-            console.log('celda 1 vacia');
+            console.log('celda 1 vacia, selecciona una opcion');
         }
     }
 
     Celda2 = () => {
-        if (this.state.contador === 1) {
+        if (this.state.ps1Play === 1) {
             console.log('celda 2 usa X');
             this.setState({
                 celda2: 'X',
                 turno: 'X',
             });
-            document.getElementById('celda2').style.backgroundColor = '#00FF00';
+            document.getElementById('celda2').style.cssText = `background-image: url("${cross}");`;
+            this.IA();
         
-        } else if (this.state.contador === 2) {
+        } else if (this.state.ps1Play === 2) {
             console.log('celda 2 usa O');
             this.setState({
                 celda2: 'O',
                 turno: 'O',
             });
-            document.getElementById('celda2').style.backgroundColor = '#FF0000';
+            document.getElementById('celda2').style.cssText = `background-image: url("${circle}");`;
+            this.IA();
         
         } else {
-            console.log('celda 2 vacia');
+            console.log('celda 2 vacia, selecciona una opcion');
         }
     }
 
     Celda3 = () => {
-        if (this.state.contador === 1) {
+        if (this.state.ps1Play === 1) {
             console.log('celda 3 usa X');
             this.setState({
                 celda3: 'X',
                 turno: 'X',
             });
-            document.getElementById('celda3').style.backgroundColor = '#00FF00';
+            document.getElementById('celda3').style.cssText = `background-image: url("${cross}");`;
+            this.IA();
         
-        } else if (this.state.contador === 2) {
+        } else if (this.state.ps1Play === 2) {
             console.log('celda 3 usa O');
             this.setState({
                 celda3: 'O',
                 turno: 'O',
             });
-            document.getElementById('celda3').style.backgroundColor = '#FF0000';
+            document.getElementById('celda3').style.cssText = `background-image: url("${circle}");`;
+            this.IA();
         
         } else {
-            console.log('celda 3 vacia');
+            console.log('celda 3 vacia, selecciona una opcion');
         }
     }
 
     Celda4 = () => {
-        if (this.state.contador === 1) {
+        if (this.state.ps1Play === 1) {
             console.log('celda 4 usa X');
             this.setState({
                 celda4: 'X',
                 turno: 'X',
             });
-            document.getElementById('celda4').style.backgroundColor = '#00FF00';
+            document.getElementById('celda4').style.cssText = `background-image: url("${cross}");`;
+            this.IA();
         
-        } else if (this.state.contador === 2) {
+        } else if (this.state.ps1Play === 2) {
             console.log('celda 4 usa O');
             this.setState({
                 celda4: 'O',
                 turno: 'O',
             });
-            document.getElementById('celda4').style.backgroundColor = '#FF0000';
+            document.getElementById('celda4').style.cssText = `background-image: url("${circle}");`;
+            this.IA();
         
         } else {
-            console.log('celda 4 vacia');
+            console.log('celda 4 vacia, selecciona una opcion');
         }
     }
 
     Celda5 = () => {
-        if (this.state.contador === 1) {
+        if (this.state.ps1Play === 1) {
             console.log('celda 5 usa X');
             this.setState({
                 celda5: 'X',
                 turno: 'X',
             });
-            document.getElementById('celda5').style.backgroundColor = '#00FF00';
+            document.getElementById('celda5').style.cssText = `background-image: url("${cross}");`;
+            this.IA();
         
-        } else if (this.state.contador === 2) {
+        } else if (this.state.ps1Play === 2) {
             console.log('celda 5 usa O');
             this.setState({
                 celda5: 'O',
                 turno: 'O',
             });
-            document.getElementById('celda5').style.backgroundColor = '#FF0000';
+            document.getElementById('celda5').style.cssText = `background-image: url("${circle}");`;
+            this.IA();
         
         } else {
-            console.log('celda 5 vacia');
+            console.log('celda 5 vacia, selecciona una opcion');
         }
     }
 
     Celda6 = () => {
-        if (this.state.contador === 1) {
+        if (this.state.ps1Play === 1) {
             console.log('celda 6 usa X');
             this.setState({
                 celda6: 'X',
                 turno: 'X',
             });
-            document.getElementById('celda6').style.backgroundColor = '#00FF00';
+            document.getElementById('celda6').style.cssText = `background-image: url("${cross}");`;
+            this.IA();
         
-        } else if (this.state.contador === 2) {
+        } else if (this.state.ps1Play === 2) {
             console.log('celda 6 usa O');
             this.setState({
                 celda6: 'O',
                 turno: 'O',
             });
-            document.getElementById('celda6').style.backgroundColor = '#FF0000';
+            document.getElementById('celda6').style.cssText = `background-image: url("${circle}");`;
+            this.IA();
         
         } else {
-            console.log('celda 6 vacia');
+            console.log('celda 6 vacia, selecciona una opcion');
         }
     }
 
     Celda7 = () => {
-        if (this.state.contador === 1) {
+        if (this.state.ps1Play === 1) {
             console.log('celda 7 usa X');
             this.setState({
                 celda7: 'X',
                 turno: 'X',
             });
-            document.getElementById('celda7').style.backgroundColor = '#00FF00';
+            document.getElementById('celda7').style.cssText = `background-image: url("${cross}");`;
+            this.IA();
         
-        } else if (this.state.contador === 2) {
+        } else if (this.state.ps1Play === 2) {
             console.log('celda 7 usa O');
             this.setState({
                 celda7: 'O',
                 turno: 'O',
             });
-            document.getElementById('celda7').style.backgroundColor = '#FF0000';
+            document.getElementById('celda7').style.cssText = `background-image: url("${circle}");`;
+            this.IA();
         
         } else {
-            console.log('celda 7 vacia');
+            console.log('celda 7 vacia, selecciona una opcion');
         }
     }
 
     Celda8 = () => {
-        if (this.state.contador === 1) {
+        if (this.state.ps1Play === 1) {
             console.log('celda 8 usa X');
             this.setState({
                 celda8: 'X',
                 turno: 'X',
             });
-            document.getElementById('celda8').style.backgroundColor = '#00FF00';
+            document.getElementById('celda8').style.cssText = `background-image: url("${cross}");`;
+            this.IA();
         
-        } else if (this.state.contador === 2) {
+        } else if (this.state.ps1Play === 2) {
             console.log('celda 8 usa O');
             this.setState({
                 celda8: 'O',
                 turno: 'O',
             });
-            document.getElementById('celda8').style.backgroundColor = '#FF0000';
+            document.getElementById('celda8').style.cssText = `background-image: url("${circle}");`;
+            this.IA();
         
         } else {
-            console.log('celda 8 vacia');
+            console.log('celda 8 vacia, selecciona una opcion');
         }
     }
 
     Celda9 = () => {
-        if (this.state.contador === 1) {
+        if (this.state.ps1Play === 1) {
             console.log('celda 9 usa X');
             this.setState({
                 celda9: 'X',
                 turno: 'X',
             });
-            document.getElementById('celda9').style.backgroundColor = '#00FF00';
+            document.getElementById('celda9').style.cssText = `background-image: url("${cross}");`;
+            this.IA();
         
-        } else if (this.state.contador === 2) {
+        } else if (this.state.ps1Play === 2) {
             console.log('celda 9 usa O');
             this.setState({
                 celda9: 'O',
                 turno: 'O',
             });
-            document.getElementById('celda9').style.backgroundColor = '#FF0000';
+            document.getElementById('celda9').style.cssText = `background-image: url("${circle}");`;
+            this.IA();
         
         } else {
-            console.log('celda 9 vacia');
+            console.log('celda 9 vacia, selecciona una opcion');
         }
     }
 
     IA = () => {
-        console.log('programar IA');
+        if (this.state.ia === 1) {
+            console.log('IA seleccionada como jugador contrario, IA hace algo');
+        }
     }
 
     Player2 = () => {
@@ -272,7 +295,7 @@ export default class TicTacToe extends Component {
                     </form>
                     <form>
                         <label htmlFor="ia">Vs IA</label>
-                        <input type="radio" name="chose" id="ia" onClick={this.VsIa}/>
+                        <input type="radio" name="chose" id="ia" onClick={this.VsIa} />
                         <label htmlFor="player">Vs Player</label>
                         <input type="radio" name="chose" id="player" onClick={this.VsPlayer}/>
                     </form>
