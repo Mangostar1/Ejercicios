db1ceaa392e49f40d5e0e3f9bc19e63904e5a090
Omar Zavala <influencia.x.94@gmail.com>
2022-07-08 22:42:30 -0400

    Se agregan estilos

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,18 @@
-/* import 'bootstrap/dist/css/bootstrap.min.css'; */
+import './Header.css';
 
 export default function Header() {
     return(
-        <header>
-            <nav>
-                <div>
-                    <h4>Logo</h4>
+        <header className="header">
+            <nav className="nav">
+                <div className="logo-content">
+                    <a className="link-logo" href="https://omar-zavala.xyz/">
+                        <img src="https://picsum.photos/80/25" />
+                    </a>
                 </div>
-                <ul>
-                    <li><a>Link 1</a></li>
-                    <li><a>Link 2</a></li>
-                    <li><a>Link 3</a></li>
+                <ul className="menu">
+                    <li className="list"><a className="link" href="#">Inicio</a></li>
+                    <li className="list"><a className="link" href="#">Link 2</a></li>
+                    <li className="list"><a className="link" href="#">Link 3</a></li>
                 </ul>
             </nav>
         </header>
