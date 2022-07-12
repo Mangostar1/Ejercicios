0bab7d97a4aed0c6228526f94b263275bd082239
Omar Zavala <influencia.x.94@gmail.com>
2022-07-12 17:14:39 -0400

    Se agrega nuevo componente en la seccion de APIs

--- src/APIs.js
+++ src/APIs.js
@@ -1,12 +1,11 @@
+import PokeAPI from "Componentes/pokeAPI";
 import './APIs.css';
 
 export default function APIs() {
     return(
         <main id="main">
             <h1>APIs</h1>
-            <p>
-                En esta seccion se trabajaran pequeños proyectos que consumiran APIs.
-            </p>
+            <PokeAPI />
         </main>
     );
 }
---
+++ src/Componentes/pokeAPI.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+import 'css/pokeAPI.css';
+
+export default function PokeAPI() {
+    return(
+        <section className="poke-api">
+            <h1>PokeAPI</h1>
+        </section>
+    );
+}
