1d3529beee02037de91855e58bb5a49ea0cd1f9e
Omar Zavala <influencia.x.94@gmail.com>
2022-07-12 20:58:51 -0400

    Se agrega fetch en pokeapi

--- src/APIs.js
+++ src/APIs.js
@@ -1,10 +1,12 @@
+import React,{useState} from 'react';
+
 import PokeAPI from "Componentes/pokeAPI";
 import './APIs.css';
 
 export default function APIs() {
     return(
         <main id="main">
-            <h1>APIs</h1>
+            <h1 id="title-main">APIs</h1>
             <PokeAPI />
         </main>
     );
--- src/Componentes/pokeAPI.js
+++ src/Componentes/pokeAPI.js
@@ -2,9 +2,27 @@ import React, {useState, useEffect} from 'react';
 import 'css/pokeAPI.css';
 
 export default function PokeAPI() {
+    async function FetchItem() {
+        let pokeSearch = document.getElementById('pokemonSearch').value;
+        let data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeSearch}`);
+        let pokemon = await data.json();
+        console.log(pokemon);
+    }
+    useEffect(() => {
+        FetchItem();
+    }, []);
+
+    const Test = () => {
+        FetchItem();
+    }
     return(
         <section className="poke-api">
-            <h1>PokeAPI</h1>
+            <h2>PokeAPI</h2>
+            <form className="poke-api-form">
+                <label htmlFor="pokemonSearch">Buscar Pokemon</label>
+                <input type="text" id="pokemonSearch" placeholder="Pokemon" />
+                <input type="button" value="Buscar" onClick={Test}/>
+            </form>
         </section>
     );
 }
