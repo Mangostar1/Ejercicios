d24236a005fe410580629bfcb2d67328c5e05764
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-07-23 20:59:05 -0400

    Se avanza en pokeAPI

--- src/Componentes/pokeAPI.js
+++ src/Componentes/pokeAPI.js
@@ -1,28 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import 'css/pokeAPI.css';
 
+function Poke({name, avatar, id}) {
+    return(
+        <>
+            <h1>PokeAPI</h1>
+        </>
+    );
+}
+
 export default function PokeAPI() {
-    const [pokemon, setPokemon] = useState({});
+    const [pokemons, setPokemons] = useState([]);
+    const [pokemonNameF, setPokemonNameF] = useState('pikachu');
+
     async function FetchItem() {
-        let pokeSearch = document.getElementById('pokemonSearch').value;
-        let data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeSearch}`);
+        let data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNameF}`);
         let pokemon = await data.json();
-        console.log(pokemon);
+        setPokemons(pokemon);
     }
     useEffect(() => {
         FetchItem();
     }, []);
 
     const Test = () => {
-        console.log('Hola');
-    }
+        setPokemonNameF(document.getElementById('pokemonSearch').value);
+        console.log(pokemonNameF, pokemons);
+    }// agregar elementos html para mostrar datos del json, la info del pokemon consultado a la api
     return(
         <section className="poke-api">
             <h2>PokeAPI</h2>
             <form className="poke-api-form">
                 <label htmlFor="pokemonSearch">Buscar Pokemon</label>
-                <input type="text" id="pokemonSearch" placeholder="Pokemon" defaultValue="pikachu" />
-                <input type="button" value="Buscar" onClick={Test} className='' />
+                <input type="text" id="pokemonSearch" placeholder="Pokemon" className="py-1 px-2"/>
+                <input type="button" value="Buscar" onClick={Test} className='bg-sky-50 hover:bg-sky-200 py-1 px-2' />
             </form>
         </section>
     );
