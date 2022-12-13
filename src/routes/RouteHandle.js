bfdf51ec1b4562983a5c647b9e18df7cfd866e1a
Omar Zavala <influencia.x.94@gmail.com>
2022-12-12 23:51:05 -0300

    Se agrega componente para controlar errores 404 al buscar rutas

--- src/routes/RouteHandle.js
+++ src/routes/RouteHandle.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from "../pages/mainPage/Main";
 import APIs from '../pages/apisPage/APIs';
+import Error404 from 'pages/404/Error404';
 
 export default function RouteHandle() {
     return(
@@ -9,7 +10,7 @@ export default function RouteHandle() {
             <Routes>
                 <Route path='/' element={<Main />}/>
                 <Route path='/apis' element={<APIs />}/>
-                <Route path='*' element={<h1> Error 404</h1>}/>
+                <Route path='*' element={<Error404 />}/>
             </Routes>
         </BrowserRouter>
     );
