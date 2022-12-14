2014def6627beaa168abeb527a75537fad21563e
Omar Zavala <influencia.x.94@gmail.com>
2022-12-13 23:14:17 -0300

    se implementa el componente footer

--- src/pages/apisPage/APIs.js
+++ src/pages/apisPage/APIs.js
@@ -3,10 +3,15 @@ import React from 'react';
 import PokeAPI from "pages/apisPage/API_projects/pokeAPI";
 import './APIs.css';
 
+import Footer from "../../components/Footer";
+
 export default function APIs() {
     return(
-        <main id="main-api">
-            <PokeAPI />
-        </main>
+        <>
+            <main id="main-api">
+                <PokeAPI />
+            </main>
+            <Footer />
+        </>
     );
 }
--- src/pages/mainPage/Main.js
+++ src/pages/mainPage/Main.js
@@ -5,13 +5,18 @@ import Calorias from './mini_projects/Calorias';
 import TicTacToe from './mini_projects/TicTacToe';
 import Paint from './mini_projects/Paint';
 
+import Footer from "../../components/Footer";
+
 export default function Main() {
     return (
-        <main id="main">
-            <Control 
-                componentName_1="Calculadora" componentName_2="Calorias" componentName_3="Tic Tac Toe" componentName_4="Paint" 
-                component_1={<Calculadora />} component_2={<Calorias />} component_3={<TicTacToe />} component_4={<Paint />}
-            />
-        </main>
+        <>
+            <main id="main">
+                <Control 
+                    componentName_1="Calculadora" componentName_2="Calorias" componentName_3="Tic Tac Toe" componentName_4="Paint" 
+                    component_1={<Calculadora />} component_2={<Calorias />} component_3={<TicTacToe />} component_4={<Paint />}
+                />
+            </main>
+            <Footer />
+        </>
     );
 }
