1fd446c803bc390fafc7ce20294dc024ed8f4abb
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-12-14 14:52:44 -0300

    Se guardan cambios en el componente login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,23 +2,26 @@ import React, { useState, useEffect } from 'react';
 
 export default function Login() {
     
-    const [user, setUser] = useState(null);
-    const [password, setPassword] = useState(null);
+    const [body, setBody] = useState({username: '', password: ''});
     
-    const handleLogin = () => {
-        let userInput = document.querySelector('#userInput').value;
-        let passwordInput = document.querySelector('#passwordInput').value;
-        setUser(userInput);
-        setPassword(passwordInput);
-        console.log(user, password);
+    const inputChange = ({target}) => {
+        const {name, value} = target
+        setBody({
+            ...body,
+            [name]: value
+        })
+    }
+
+    const sendLogin = () => {
+        console.log(body.username, body.password);
     }
 
     return(
         <main>
             <form className="">
-                <input type="text" placeholder="User" id="userInput" className="bg-gray-100 py-1 px-2" />
-                <input type="password" placeholder="Password" id="passwordInput" className="bg-gray-100 py-1 px-2" />
-                <input type="button" value="Login" onClick={handleLogin} className="bg-gray-300 py-1 px-2" />
+                <input type="text" placeholder="User" value={body.username} onChange={inputChange} name='username' className="bg-gray-100 py-1 px-2" />
+                <input type="password" placeholder="Password" value={body.password} onChange={inputChange} name='password' className="bg-gray-100 py-1 px-2" />
+                <input type="button" value="Login" onClick={sendLogin} className="bg-gray-300 py-1 px-2" />
             </form>
         </main>
     )
