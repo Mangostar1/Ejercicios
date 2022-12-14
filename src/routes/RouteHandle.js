2f1c6ac4be1d5da83187c7ea5624c8a4080a94d0
Omar Zavala <influencia.x.94@gmail.com>
2022-12-13 23:34:40 -0300

    Se crean nuevas rutas

--- src/routes/RouteHandle.js
+++ src/routes/RouteHandle.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from "../pages/mainPage/Main";
 import APIs from '../pages/apisPage/APIs';
+import Login from '../pages/login/Login';
+import Dashboard from '../pages/dashboard/Dashboard';
 import Error404 from 'pages/404/Error404';
 
 export default function RouteHandle() {
@@ -10,6 +12,8 @@ export default function RouteHandle() {
             <Routes>
                 <Route path='/' element={<Main />}/>
                 <Route path='/apis' element={<APIs />}/>
+                <Route path='/login' element={<Login />}/>
+                <Route path='/dashboard' element={<Dashboard />}/>
                 <Route path='*' element={<Error404 />}/>
             </Routes>
         </BrowserRouter>
