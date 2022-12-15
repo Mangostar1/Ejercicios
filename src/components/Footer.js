d1242f989fc68f1b728a6d7b73915d5d2f57a8dc
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-12-15 12:56:09 -0300

    se guardan cambios de estilos en login y correcion de sintaxys en componente footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,23 +9,23 @@ export default function Footer() {
     return(
         <footer>
             <div id="git-content-footer">
-            <a id="github-link" class="personalLinks" href="https://github.com/Mangostar1" target="_blank" rel="noopener noreferrer">
-                <img src={githubIMG} class="github" id="github" title="GitHub" alt="my github" />
+            <a id="github-link" className="personalLinks" href="https://github.com/Mangostar1" target="_blank" rel="noopener noreferrer">
+                <img src={githubIMG} className="github" id="github" title="GitHub" alt="my github" />
             </a>
         </div>
         <div id="miweb-content-footer">
-            <a id="my-web" class="personalLinks" href="https://omar-zavala.xyz" target="_blank" rel="noopener noreferrer">
-                <img src={myPageIMG} class="github" id="miweb" title="omar-zavala.xyz" alt="my web" />
+            <a id="my-web" className="personalLinks" href="https://omar-zavala.xyz" target="_blank" rel="noopener noreferrer">
+                <img src={myPageIMG} className="github" id="miweb" title="omar-zavala.xyz" alt="my web" />
             </a>
         </div>
         <div id="linkedin-content-footer">
-            <a href="https://www.linkedin.com/in/omar-zavala-ugarte-7b5036149/" class="personalLinks" target="_blank" rel="noopener noreferrer">
-                <img src={linkedinIMG} class="linkedin" id="linkedin" title="Linkedin" alt="linkedin" />
+            <a href="https://www.linkedin.com/in/omar-zavala-ugarte-7b5036149/" className="personalLinks" target="_blank" rel="noopener noreferrer">
+                <img src={linkedinIMG} className="linkedin" id="linkedin" title="Linkedin" alt="linkedin" />
             </a>
         </div>
         <div id="email-content-footer">
-            <a href="mailto:influencia.x.94@gmail.com" class="personalLinks" target="_blank" rel="noreferrer">
-                <img src={emailIMG} class="email" id="email" title="e-mail" alt="my gmail" />
+            <a href="mailto:influencia.x.94@gmail.com" className="personalLinks" target="_blank" rel="noreferrer">
+                <img src={emailIMG} className="email" id="email" title="e-mail" alt="my gmail" />
             </a>
         </div>
         </footer>
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -14,21 +14,43 @@ export default function Login() {
         })
     }
 
-    const sendLogin = () => {
+    const sendLogin = async() => {
         console.log(body.username, body.password);
+
+        try {
+            await fetch('https://learn-backend-node-express-production.up.railway.app/api/login', {
+                method: 'POST',
+                headers: { 
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "username": `${body.username}`,
+                    "password": `${body.password}`
+                })
+            });
+        } catch (error) {
+            console.error(error, 'Ocurrio un error 😒');
+        }
+
     }
 
     return(
         <>
             <main className=''>
-                <form className="bg-red-600 flex flex-col items-center justify-center px-6 py-8 w-60 m-auto">
-                    <input type="text" placeholder="User" value={body.username} onChange={inputChange} name='username' className="bg-gray-100 py-1 px-2 border-solid border-1 border-light-blue-500" />
-                    <input type="password" placeholder="Password" value={body.password} onChange={inputChange} name='password' className="bg-gray-100 py-1 px-2 border-solid border-1 border-light-blue-500" />
-                    <input type="button" value="Login" onClick={sendLogin} className="bg-gray-300 hover:bg-gray-400 py-1 px-2" />
+                <form className="bg-gray-200 flex flex-col items-center justify-center px-6 py-8 w-60 m-auto gap-2">
+                    <div>
+                        <label htmlFor='user' className=''>User:</label>
+                        <input type="text" placeholder="User" value={body.username} onChange={inputChange} name='username' id='user' className="bg-gray-100 py-1 px-2 w-full border rounded border-gray-300 focus:border-green-600 focus:outline-none" />
+                    </div>
+
+                    <div>
+                        <label htmlFor='pass' className=''>Password:</label>
+                        <input type="password" placeholder="Password" value={body.password} onChange={inputChange} name='password' id='pass' className="bg-gray-100 py-1 px-2 w-full border rounded border-gray-300 focus:border-green-600 focus:outline-none" />
+                    </div>
+                    <input type="button" value="Login" onClick={sendLogin} className="transition duration-500 ease-in-out bg-gray-300 hover:bg-gray-400 py-1 px-2 w-full rounded" />
                 </form>
             </main>
             <Footer />
         </>
-
     )
 }
