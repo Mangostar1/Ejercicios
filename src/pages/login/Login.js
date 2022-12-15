5aae31aa08efd90ea17149f8b051f81d5b85780f
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-12-14 22:30:40 -0300

    Se guardan cambios en estilos del login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+
+import Footer from "components/Footer";
 
 export default function Login() {
     
@@ -17,12 +19,16 @@ export default function Login() {
     }
 
     return(
-        <main>
-            <form className="">
-                <input type="text" placeholder="User" value={body.username} onChange={inputChange} name='username' className="bg-gray-100 py-1 px-2" />
-                <input type="password" placeholder="Password" value={body.password} onChange={inputChange} name='password' className="bg-gray-100 py-1 px-2" />
-                <input type="button" value="Login" onClick={sendLogin} className="bg-gray-300 py-1 px-2" />
-            </form>
-        </main>
+        <>
+            <main className=''>
+                <form className="bg-red-600 flex flex-col items-center justify-center px-6 py-8 w-60 m-auto">
+                    <input type="text" placeholder="User" value={body.username} onChange={inputChange} name='username' className="bg-gray-100 py-1 px-2 border-solid border-1 border-light-blue-500" />
+                    <input type="password" placeholder="Password" value={body.password} onChange={inputChange} name='password' className="bg-gray-100 py-1 px-2 border-solid border-1 border-light-blue-500" />
+                    <input type="button" value="Login" onClick={sendLogin} className="bg-gray-300 hover:bg-gray-400 py-1 px-2" />
+                </form>
+            </main>
+            <Footer />
+        </>
+
     )
 }
