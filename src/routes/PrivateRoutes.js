b3972f858a071fd50349cd7e52046f53ea58ac85
Omar Zavala <influencia.x.94@gmail.com>
2022-12-21 22:23:41 -0300

    Se finalizan trabajos en PrivateRoutes, ahora si el usuario no esta logeado, no puede acceder al dashboard

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,14 +1,12 @@
-import { useNavigate, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export default function PrivateRoutes({children}) {
-
-    const navigate = useNavigate();
-    const location = useLocation();
 
     if (localStorage.hasOwnProperty('auth') === true) {
         console.log("el usuario esta logeado");
         return children
     }
-    /* location.replace('/login') */
-    return navigate("/login")
+
+    return <Navigate to="/login" />
+
 }
