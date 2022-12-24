ffa1e13155fafe8730548991d43b08c86ee3b931
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-12-24 18:38:38 -0300

    Se finaliza la construccion de la logica del login.

--- src/components/LoginHandle.js
+++ src/components/LoginHandle.js
@@ -2,8 +2,12 @@ import { Link } from 'react-router-dom';
 
 export default function LoginHandle() {
 
+    const logout = () => {
+        localStorage.clear();
+    }
+
     if (localStorage.hasOwnProperty('auth') === true) {
-        return<Link to='/' reloadDocument>Logout</Link>
+        return<Link to='/' onClick={logout} reloadDocument>Logout</Link>
     }
     return <Link to='/login'>Login</Link>
 }
