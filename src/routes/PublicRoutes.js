2ee91fd58d0638b015858b050eb10faca40b2ca7
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-03-12 20:50:32 -0300

    Se corrige sintaxis del nuevo header, se elimina archivo .js innecesario

--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -14,27 +14,36 @@ import LoginHandle from 'components/LoginHandle';
 
 export default function PublicRoutes() {
 
+    const handleNavView = () => {
+        const menu = document.querySelector('#menu');
+        menu.classList.toggle('hidden');
+    }
+
+
     return(
             <BrowserRouter>
 
                 <header className="header">
-                    <nav className="nav">
-                        <div className="logo-content">
-                            <a className="link-logo" href="https://omar-zavala.xyz/">
-                                <img id='logo' src={logo} alt='logo'/>
-                            </a>
+                    <nav className="flex flex-wrap items-center justify-between w-full py-4 md:py-0 px-4 text-lg text-gray-700 bg-white">
+                        <div>
+                        <a href="https://omar-zavala.xyz/">
+                            <img id='logo' src={logo} alt='logo'/>
+                        </a>
                         </div>
-                        <div className='menu-content'>
-                            <ul className="menu">
-                                <li id="nav-home" className="link" >
-                                    <Link to='/'>Inicio</Link>
+                
+                        <svg xmlns="http://www.w3.org/2000/svg" id="menu-button" className="h-6 w-6 cursor-pointer md:hidden block" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={handleNavView}>
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
+                        </svg>
+                
+                        <div className="hidden w-full md:flex md:items-center md:w-auto z-30" id="menu">
+                            <ul className="pt-4 text-base text-gray-700 md:flex md:justify-between md:pt-0">
+                                <li>
+                                    <Link to='/' className="md:p-4 py-2 block hover:text-purple-400">Inicio</Link>
                                 </li>
-                                <li id="nav-about" className="link" >
-                                    <Link to='/apis'>APIs</Link>
+                                <li>
+                                    <Link to='/apis' className="md:p-4 py-2 block hover:text-purple-400">APIs</Link>
                                 </li>
-                            </ul>
-                            <ul className="menu">
-                                <li id="nav-home" className="link" >
+                                <li>
                                     <LoginHandle />
                                 </li>
                             </ul>
